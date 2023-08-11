8eabad4589203fb3e83bb774973b0cf5b23ddbbc
Koka Easwarendra <easwarendra.ece@gmail.com>
2023-08-11 14:15:33 +0530

    deploy

--- sst.config.ts
+++ sst.config.ts
@@ -1,25 +1,32 @@
+import { Certificate } from "aws-cdk-lib/aws-certificatemanager";
 import { type SSTConfig } from "sst";
 import { NextjsSite } from "sst/constructs";
 
 export default {
   config(_input) {
     return {
-      name: "newt",
+      name: "website-2",
       region: "us-east-2",
     };
   },
   stacks(app) {
     app.stack(function Site({ stack }) {
       const site = new NextjsSite(stack, "site", {
-        // customDomain: "qnext.ai",
         customDomain: {
-          domainName: "site.qnext.ai",
-          hostedZone: "qnext.ai",
+          isExternalDomain: true,
+          domainName: "qnext.ai",
+          cdk: {
+            certificate: Certificate.fromCertificateArn(
+              stack,
+              "11AugCert",
+              "arn:aws:acm:us-east-1:255823186087:certificate/eff5ce75-d6b9-4467-93cd-0a2898f9b996"
+            ),
+          },
         },
       });
 
       stack.addOutputs({
-        SiteUrl: site.customDomainUrl || site.url,
+        SiteUrl: site.url,
       });
     });
   },
